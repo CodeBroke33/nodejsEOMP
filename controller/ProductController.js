5fb336508bc1a0f51a9248f8fd07f4b2d6a9fcfb
Understudystudent <carlmjames2@gmail.com>
2024-02-26 12:52:27 +0200

    made the fecth products

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -1,1 +1,21 @@
-///
+import express from 'express';
+import bodyParser from 'body-parser';
+import { products } from '../model/index.js';
+
+const productRouter = express.Router();
+
+productRouter.get('/', (req, res)=>{
+    try {
+        products.fetchProducts(req, res);
+    } catch(e){
+        res.json({
+            status: res.statusCode,
+            msg: "Failed to retrieve users"
+        })
+    }
+})
+
+
+export {
+    productRouter
+}
