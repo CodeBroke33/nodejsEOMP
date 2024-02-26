c7b07a21f2a37014bef7afeeeb285516b7f493ab
Understudystudent <carlmjames2@gmail.com>
2024-02-26 13:32:08 +0200

    made the productFetch fuction

--- model/Product.js
+++ model/Product.js
@@ -1,1 +1,23 @@
-//
+import {connection as db} from "../config/index.js"
+
+
+class Products{
+    fetchProducts(req, res){
+        const qry = `
+        SELECT prodID, prodName, prodQuantity,
+        prodAmount, userID
+        FROM Products;
+        `
+        db.query(qry, (err, results)=>{
+            if(err) throw err 
+            res.json({
+                status: res.statusCode,
+                results
+            })
+        })
+    }
+}
+
+export {
+    Products
+}
