0d5c375149f9a8c8083c08ca03dbdb5267d084d9
CodeBroke33 <minedom33@gmail.com>
2024-02-27 16:31:08 +0200

    update

--- nodejseompfrontend/src/views/HomeView.vue
+++ nodejseompfrontend/src/views/HomeView.vue
@@ -1,70 +1,82 @@
 <template>
-  <div class="homepage bg-black bg-image">
+  <div class="homepage bg-black bg-image" @click="createRipple">
     <h1 class="home text-black margin-top">WELCOME<br>TO<br></h1>
     <img class="img-fluid rounded-circle" type="image" src="https://i.ibb.co/0M1KHF7/2-removebg-preview.png" alt="logo">
+    <transition-group name="ripple">
+      <span v-for="(ripple, index) in ripples" :key="index" class="ripple"></span>
+    </transition-group>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeView',
-  
+  data() {
+    return {
+      ripples: [],
+    };
+  },
+  methods: {
+    createRipple(event) {
+      const rect = event.target.getBoundingClientRect();
+      const size = Math.max(rect.width, rect.height);
+      const x = event.clientX - rect.left - size / 2;
+      const y = event.clientY - rect.top - size / 2;
+
+      this.ripples.push({ x, y });
+    },
+  },
 };
 </script>
 
 <style scoped>
- @import url('https://fonts.googleapis.com/css2?family=Jacques+Francois+Shadow&display=swap');
- 
- *,
- ::after,
- ::before {
+@import url('https://fonts.googleapis.com/css2?family=Jacques+Francois+Shadow&display=swap');
+
+*,
+::after,
+::before {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  /* overflow: hidden; */
-  /* background-image: url('https://i.ibb.co/MPfYxHn/WATER-H2-O-2.jpg'); */
-  /* You can also use other background properties such as size, position, repeat, etc. */
-  /* background-size: cover;
-  background-position: center; */
 }
-/* 
-.homepage{
-  background-color: black;
-}  */
 
 h1 {
   font-family: 'Jacques Francois Shadow', cursive;
-  font-size: 5rem; 
-  /* position: absolute;
-  bottom: 22%;
-  right: 3.5em; */
+  font-size: 5rem;
 }
 
 .bg-image {
-  background-image: url('https://i.ibb.co/MPfYxHn/WATER-H2-O-2.jpg'), linear-gradient(to top, rgba(0,0,0,0.8), rgba(255,255,255,0));
-  background-size: cover;
-  /* You can also use other background properties such as size, position, repeat, etc. */
+  background-image: url('https://i.ibb.co/MPfYxHn/WATER-H2-O-2.jpg');
   background-size: cover;
   background-position: center;
   height: auto;
-  position: relative;
-  height: 100vh;
-  background-image: linear-gradient(to top, rgba(0,0,0,0.8), rgba(255,255,255,0));
-  /* This will create a linear gradient that goes from black (0,0,0) with 80% opacity at the bottom to transparent white (255,255,255) at the top */
-
 }
 
 .homepage {
   padding: 8%;
 }
 
-.homepage h1 {
-  font-size: 7rem; /* Initial font size */
+@media (max-width: 768px) {
+  .homepage h1 {
+    font-size: 3rem;
+  }
 }
 
-@media (max-width: 768px) {
-  .homepage h1 bg {
-    font-size: 3rem; /* Adjust font size for smaller screens */
+.ripple {
+  position: absolute;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 50%;
+  transform: scale(0);
+}
+
+.ripple-enter-active {
+  animation: rippleEffect 0.6s linear;
+}
+
+@keyframes rippleEffect {
+  to {
+    transform: scale(4);
+    opacity: 0;
   }
 }
 </style>
