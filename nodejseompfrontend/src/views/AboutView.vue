0a8830abf0d4e0382843c8b8987e8b1cac46bdab
CodeBroke33 <146824177+CodeBroke33@users.noreply.github.com>
2024-02-27 13:45:53 +0200

    Update AboutView.vue

--- nodejseompfrontend/src/views/AboutView.vue
+++ nodejseompfrontend/src/views/AboutView.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="aboutpage bg-black">
+  <main class="bg-black">
+    <div class="aboutpage">
     <h1 id="header" class="fs-1 mb-4">Meet The Team</h1>
-    <div class="container">
+</div>
+
+<div class="about">
+  <div class="container">
                 <div class="row">
                     <div class="col">
-                        <img src="" alt="">
+                        <h1>Razien Mackenzie</h1>
+                        <p>
+                            Razien Mackenzie is defined by his remarkable determination and ambition. His relentless pursuit of goals is fueled by unwavering dedication, pushing him forward with enthusiasm. Razien's infectious optimism uplifts those around him, instilling a resilient mindset that tackles challenges with boundless energy.<br><br>With a reputation for reliability and independence, Razien, as the Chief Information Officer (CIO) of the company, takes charge confidently, navigating through obstacles with ease. His leadership qualities extend beyond mere direction, as he leads by example, inspiring others to rise to their full potential. In any environment, Razien's presence is not only notable but also transformative, leaving a lasting impact on those he encounters.</p>
                     </div>
                     <div class="col">
-                        <h1></h1>
-                        <p></p>
+                        <img src="https://i.ibb.co/0ZB2CJ2/default-avatar-profile-icon-vector.jpg" class="w-20 h-25" alt="Profile">
+                    </div>
+                </div>
+            </div>
+            <!-- Carl -->
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1>Carl Micheal James</h1> 
+                        <p>
+                            Carl Michael James epitomizes an exceptional individual, showcasing a blend of resolute qualities and versatile skills. He exudes determination and confidence, demonstrating autonomy and initiative in his endeavors. With unwavering reliability, Carl Michael consistently fulfills his responsibilities, making decisive contributions to problem-solving and progress.<br><br>His insightful perspectives and entrepreneurial mindset foster innovation and strategic thinking, while his role as CFO underscores his financial acumen and leadership prowess. Carl Michael's dynamic persona enriches every project, making collaboration with him both enriching and rewarding.</p>
+                    </div>
+                    <div class="col">
+                        <img src="https://i.ibb.co/0ZB2CJ2/default-avatar-profile-icon-vector.jpg" class="w-20 h-25"  alt="Profile">
                     </div>
                 </div>
             </div>
 </div>
+  </main>
 
 </template>
 
@@ -32,7 +51,7 @@
  ::after,
  ::before{
   margin: 0;
-  padding: 0.3%;
+  padding: 1.5%;
   box-sizing: border-box;
  }
 
@@ -44,13 +63,23 @@
 
 #header {
   font-family: 'Jacques Francois Shadow', cursive;
-  font-size: 6rem !important;
+  font-size: 7rem !important;
   color: white;
-  padding: 0.3%;
+  padding: 17%;
+}
+
+ h1 {
+  font-family: 'Jacques Francois Shadow', cursive;
+  font-size: 2rem !important;
+  color: white;
+  padding: 1%;
 }
 
 p {
-  position: absolute;
-  bottom: 15em;
+  font-size: 0.8em;
+}
+
+.about {
+  margin-left: 5%
 }
 </style>
