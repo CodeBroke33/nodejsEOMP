eedf84cb3f6e25b888aaa5277e642bd50ac2db77
Understudystudent <carlmjames2@gmail.com>
2024-02-27 15:20:01 +0200

    working on admin page

---
+++ nodejseompfrontend/src/users-temp-data.js
@@ -0,0 +1,39 @@
+
+
+export const usersData = {
+    users: [
+      {
+        userID: 1,
+        firstName: "Carl",
+        lastName: "James",
+        userAge: 25,
+        Gender: "female",
+        emailAdd: "someemailvsb4@gmaild.com",
+        userPass: "156egghf",
+        userRole: "user",
+      },
+      {
+        userID: 2,
+        firstName: "Alice",
+        lastName: "Smith",
+        userAge: 30,
+        Gender: "male",
+        emailAdd: "alice.smith@email.com",
+        userPass: "7d8s92j",
+        userRole: "admin",
+
+      },
+      {
+        userID: 3,
+        firstName: "Eva",
+        lastName: "Johnson",
+        userAge: 22,
+        Gender: "female",
+        emailAdd: "eva.johnson@email.com",
+        userPass: "p@ssw0rd",
+        userRole: "user",
+
+      }
+    ]
+  };
+  
