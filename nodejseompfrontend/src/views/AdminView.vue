c00874444dcb45d48d09a9fff8224353a9f0af36
Understudystudent <carlmjames2@gmail.com>
2024-02-28 16:10:27 +0200

    update style on admin

--- nodejseompfrontend/src/views/AdminView.vue
+++ nodejseompfrontend/src/views/AdminView.vue
@@ -1,125 +1,113 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <h2 class="display-4">User CRUD</h2>
-        </div>
-        <div class="row">
-            <div class="col">
-                <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="">Add </button>
-            </div>
-        </div>
-        <div class="row">
-            <table>
-                <thead>
-                    <tr>
-                        <th>User ID</th>
-                        <th>First name</th>
-                        <th>Last name</th>
-                        <th>User age</th>
-                        <th>Gender</th>
-                        <th>Email address</th>
-                        <th>User role</th>
-                        <th>
-                            Action
-                        </th>
-                    </tr>
-                </thead>
-                <tbody v-if="users">
-                    <tr v-for="user in users" :key="user.userID">
-                        <td>
-                            {{ user.userID }}
-                        </td>
-                        <td>
-                            {{ user.firstName }}
-                        </td>
-                        <td>
-                            {{ user.lastName }}
-                        </td>
-                        <td>
-                            {{ user.userAge }}
-                        </td>
-                        <td>
-                            {{ user.gender }}
-                        </td>
-                        <td>
-                            {{ user.emailAdd }}
-                        </td>
-                        <td>
-                            {{ user.userRole }}
-                        </td>
-                        <td class="d-flex justify-content-between">
-                            <button class="btn btn-success">Edit</button>
-                            <button class="btn btn-success">Delete</button>
-
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="row">
-            <h2 class="display-4">Product CRUD</h2>
-        </div>
-        <div class="row">
-            <div class="col">
-                <button class="btn btn-success">Add </button>
-            </div>
-        </div>
-        <div class="row">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Product ID</th>
-                        <th>Product Name</th>
-                        <th>Product Quantity</th>
-                        <th>Product Amount</th>
-                        <th>
-                            Action
-                        </th>
-                    </tr>
-                </thead>
-                <tbody v-if="products">
-                    <tr v-for="product in products" :key="product.prodID">
-                        <td>
-                            {{ product.prodID }}
-                        </td>
-                        <td>
-                            {{ product.prodName }}
-                        </td>
-                        <td>
-                            {{ product.prodQuantity }}
-                        </td>
-                        <td>
-                            R {{ product.prodAmount }}
-                        </td>
-                        <td class="d-flex justify-content-between">
-                            <button class="btn btn-success">Edit</button>
-                            <button class="btn btn-success">Delete</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+  <div class="adminpage">
+    <div class="col">
+      <h2 class="admin">User CRUD</h2>
+      <div class="row">
+        <!-- <div class="col">
+          <button class="subadmin btn btn-light">Add User</button>
+        </div> -->
+      </div>
+      <table class="table table-striped-columns">
+        <thead>
+          <tr>
+            <th>User ID</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>User age</th>
+            <th>Gender</th>
+            <th>Email address</th>
+            <th>User role</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody v-if="users">
+          <tr v-for="user in users" :key="user.userID">
+            <td>{{ user.userID }}</td>
+            <td>{{ user.firstName }}</td>
+            <td>{{ user.lastName }}</td>
+            <td>{{ user.userAge }}</td>
+            <td>{{ user.gender }}</td>
+            <td>{{ user.emailAdd }}</td>
+            <td>{{ user.userRole }}</td>
+            <td>
+              <div class="row">
+                <button class="btn btn-primary w-50">Edit</button>
+                <button class="btn btn-danger w-50">Delete</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+
+    <div class="col">
+      <h2 class="admin">Product CRUD</h2>
+      <div class="row">
+        <!-- <div class="col">
+          <button class="subadmin btn btn-light">Add Product</button>
+        </div> -->
+      </div>
+      <table class="table table-striped-columns">
+        <thead>
+          <tr>
+            <th>Product ID</th>
+            <th>Product Name</th>
+            <th>Product Quantity</th>
+            <th>Product Amount</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody v-if="products">
+          <tr v-for="product in products" :key="product.prodID">
+            <td>{{ product.prodID }}</td>
+            <td>{{ product.prodName }}</td>
+            <td>{{ product.prodQuantity }}</td>
+            <td>R {{ product.prodAmount }}</td>
+            <td>
+              <div class="row">
+                <button class="btn btn-primary w-50">Edit</button>
+                <button class="btn btn-danger w-50">Delete</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        computed:{
-            users() {
-                return this.$store.state.users
-            },
-            products() {
-                return this.$store.state.products
-            }
-        },
-        mounted() {
-            this.$store.dispatch('fetchUsers')
-            this.$store.dispatch('fetchProducts')
-
-        }
-    }
+export default {
+  computed: {
+    users() {
+      return this.$store.state.users;
+    },
+    products() {
+      return this.$store.state.products;
+    },
+  },
+  mounted() {
+    this.$store.dispatch('fetchUsers');
+    this.$store.dispatch('fetchProducts');
+  },
+};
 </script>
 
-<stylescoped>
+<style  scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Jacques+Francois+Shadow&display=swap');
 
-</stylescoped>
+  .adminpage {
+    overflow-x: hidden;
+  }
+
+  .admin {
+    font-family: 'Jacques Francois Shadow', cursive;
+    font-size: 5rem;
+  }
+
+  .subadmin {
+    font-family: 'Jacques Francois Shadow', cursive;
+    font-size: 5rem;
+    border-radius: 20px;
+  }
+</style>
