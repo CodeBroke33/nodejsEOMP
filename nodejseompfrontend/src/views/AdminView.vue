b95e2315f4111051d6f5f402edd8ad3698754138
CodeBroke33 <146824177+CodeBroke33@users.noreply.github.com>
2024-03-01 15:52:22 +0200

    Update AdminView.vue

--- nodejseompfrontend/src/views/AdminView.vue
+++ nodejseompfrontend/src/views/AdminView.vue
@@ -3,87 +3,87 @@
     <div class="col">
       <h2 class="admin text-white">User</h2>
       <div class="row">
-        <!-- <router-link to="/AddUserModal">
-          <button class="btn btn-primary">Add User</button>
-        </router-link> -->
+        <!-- <div class="col">
+          <button class="subadmin btn btn-light">Add User</button>
+        </div> -->
       </div>
       <div class="container">
-        <table class="table table-striped-columns rounded-3">
-          <thead>
-            <tr>
-              <th scoped="col">User ID</th>
-              <th scoped="col">First name</th>
-              <th scoped="col">Last name</th>
-              <th scoped="col">User age</th>
-              <th scoped="col">Gender</th>
-              <th scoped="col">Email address</th>
-              <th scoped="col">User role</th>
-              <th scoped="col">Action</th>
-            </tr>
-          </thead>
-          <tbody v-if="users">
-            <tr v-for="user in users" :key="user.userID">
-              <td>{{ user.userID }}</td>
-              <td>{{ user.firstName }}</td>
-              <td>{{ user.lastName }}</td>
-              <td>{{ user.userAge }}</td>
-              <td>{{ user.gender }}</td>
-              <td>{{ user.emailAdd }}</td>
-              <td>{{ user.userRole }}</td>
-              <td>
-                <div class="row">
-                  <div class="col">
-                    <button @click="editUser(user)" class="btn btn-primary btn-block">Edit</button>
-                  </div>
-                  <div class="col">
-                    <button @click="deleteUser(user)" class="btn btn-danger btn-block">Delete</button>
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+<table class="table table-striped-columns rounded-3">
+  <thead>
+    <tr>
+      <th scoped="col">User ID</th>
+      <th scoped="col">First name</th>
+      <th scoped="col">Last name</th>
+      <th scoped="col">User age</th>
+      <th scoped="col">Gender</th>
+      <th scoped="col">Email address</th>
+      <th scoped="col">User role</th>
+      <th scoped="col">Action</th>
+    </tr>
+  </thead>
+  <tbody v-if="users">
+    <tr v-for="user in users" :key="user.userID">
+      <td>{{ user.userID }}</td>
+      <td>{{ user.firstName }}</td>
+      <td>{{ user.lastName }}</td>
+      <td>{{ user.userAge }}</td>
+      <td>{{ user.gender }}</td>
+      <td>{{ user.emailAdd }}</td>
+      <td>{{ user.userRole }}</td>
+      <td>
+        <div class="row">
+          <div class="col">
+            <button class="btn btn-primary btn-block">Edit</button>
+          </div>
+          <div class="col">
+            <button class="btn btn-danger btn-block">Delete</button>
+          </div>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table>
       </div>
     </div>
 
     <div class="col">
       <h2 class="admin text-white">Product</h2>
       <div class="row">
-        <router-link to="/AddProductModal">
-          <button class="btn btn-primary">Add Product</button>
-        </router-link>
+        <!-- <div class="col">
+          <button class="subadmin btn btn-light">Add Product</button>
+        </div> -->
       </div>
-      <div class="container">
-        <table class="table table-striped-columns rounded-5">
-          <thead>
-            <tr>
-              <th scoped="col">Product ID</th>
-              <th scoped="col">Product Name</th>
-              <th scoped="col">Product Category</th>
-              <th scoped="col">Product Amount</th>
-              <th scoped="col">Action</th>
-            </tr>
-          </thead>
-          <tbody v-if="products">
-            <tr v-for="product in products" :key="product.prodID">
-              <td><img :src="product.prodUrl" style="height: 100px;" /></td>
-              <td>{{ product.prodName }}</td>
-              <td>{{ product.Category }}</td>
-              <td>R {{ product.amount }}</td>
-              <td>
-                <div class="row">
-                  <div class="col">
-                    <button @click="editProduct(product)" class="btn btn-primary btn-block">Edit</button>
-                  </div>
-                  <div class="col">
-                    <button @click="deleteProduct(product)" class="btn btn-danger btn-block">Delete</button>
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+        <div class="container">
+          <table class="table table-striped-columns rounded-5">
+  <thead>
+    <tr>
+      <th scoped="col">Product ID</th>
+      <th scoped="col">Product Name</th>
+      <th scoped="col">Product Category</th>
+      <th scoped="col">Product Amount</th>
+      <th scoped="col">Action</th>
+    </tr>
+  </thead>
+  <tbody v-if="products">
+    <tr v-for="product in products" :key="product.prodID">
+      <td><img :src="product.prodUrl" style="height: 100px;" /></td> 
+      <td>{{ product.prodName }}</td>
+      <td>{{ product.Category }}</td>
+      <td> R {{ product.amount }}</td>
+      <td>
+        <div class="row">
+          <div class="col">
+            <button class="btn btn-primary btn-block">Edit</button>
+          </div>
+          <div class="col">
+            <button class="btn btn-danger btn-block">Delete</button>
+          </div>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table>
+        </div>
     </div>
   </div>
 </template>
@@ -98,20 +98,6 @@ export default {
       return this.$store.state.products;
     },
   },
-  methods: {
-    editUser(user) {
-      console.log(`Editing user: ${user.userID}`);
-    },
-    deleteUser(user) {
-      this.$store.dispatch('deleteUser', { id: user.userID });
-    },
-    editProduct(product) {
-      console.log(`Editing product: ${product.prodID}`);
-    },
-    deleteProduct(product) {
-      this.$store.dispatch('deleteProduct', { id: product.prodID });
-    },
-  },
   mounted() {
     this.$store.dispatch('fetchUsers');
     this.$store.dispatch('fetchProducts');
@@ -119,30 +105,38 @@ export default {
 };
 </script>
 
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Jacques+Francois+Shadow&display=swap');
+<style  scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Jacques+Francois+Shadow&display=swap');
 
-.rounded {
-  border-radius: 10px !important; 
+
+  .rounded {
+  border-radius: 10px !important; /* Adjust the value as needed */
+}
+  .adminpage {
+    overflow-x: hidden;
+  }
+
+  .admin {
+    font-family: 'Jacques Francois Shadow', cursive;
+    font-size: 5rem;
+  }
+
+  .subadmin {
+    font-family: 'Jacques Francois Shadow', cursive;
+    font-size: 5rem;
+    border-radius: 20px;
+  }
+
+  .container {
+    border-radius: 1px;
+    color: grey;
+  }
+
+th {
+  font-family: Righteous;
 }
 
-.adminpage {
-  overflow-x: hidden;
-}
-
-.admin {
-  font-family: 'Jacques Francois Shadow', cursive;
-  font-size: 5rem;
-}
-
-.subadmin {
-  font-family: 'Jacques Francois Shadow', cursive;
-  font-size: 5rem;
-  border-radius: 20px;
-}
-
-.container {
-  border-radius: 1px;
-  color: grey;
+td {
+  font-family: Righteous;
 }
 </style>
