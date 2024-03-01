cec1d1e9a0f31b94f7bb1def52ac63a937a5f4c0
Understudystudent <carlmjames2@gmail.com>
2024-03-01 15:53:08 +0200

    made admin updates

--- nodejseompfrontend/src/views/AdminView.vue
+++ nodejseompfrontend/src/views/AdminView.vue
@@ -1,148 +1,174 @@
 <template>
-  <div class="adminpage bg-black">
-    <div class="col">
-      <h2 class="admin text-white">User</h2>
-      <div class="row">
-        <!-- <router-link to="/AddUserModal">
-          <button class="btn btn-primary">Add User</button>
-        </router-link> -->
-      </div>
-      <div class="container">
-        <table class="table table-striped-columns rounded-3">
-          <thead>
-            <tr>
-              <th scoped="col">User ID</th>
-              <th scoped="col">First name</th>
-              <th scoped="col">Last name</th>
-              <th scoped="col">User age</th>
-              <th scoped="col">Gender</th>
-              <th scoped="col">Email address</th>
-              <th scoped="col">User role</th>
-              <th scoped="col">Action</th>
-            </tr>
-          </thead>
-          <tbody v-if="users">
-            <tr v-for="user in users" :key="user.userID">
-              <td>{{ user.userID }}</td>
-              <td>{{ user.firstName }}</td>
-              <td>{{ user.lastName }}</td>
-              <td>{{ user.userAge }}</td>
-              <td>{{ user.gender }}</td>
-              <td>{{ user.emailAdd }}</td>
-              <td>{{ user.userRole }}</td>
-              <td>
-                <div class="row">
-                  <div class="col">
-                    <button @click="editUser(user)" class="btn btn-primary btn-block">Edit</button>
-                  </div>
-                  <div class="col">
-                    <button @click="deleteUser(user)" class="btn btn-danger btn-block">Delete</button>
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+  <div>
+<!-- USERS TABLE -->
+<br><h2>Users Table</h2><br>
+    <button class="btn btn-primary" @click="toggleAddUser">Add User</button>
+    <addUser v-if="showAddUser"/>
+    
+    <div class="container table-responsive">
+      <table class="table">
+        <thead class="table-dark">
+          <tr>
+            <th>User ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+        <th>User Age</th>
+        <th>Gender</th>
+        <th>User Role</th>
+        <th>Email Address</th>
+        <th>User Profile</th>
+        <th>
+          Action
+        </th>
+      </tr>
+    </thead>
+  <tbody v-if="users">
+<tr v-for="user in users" :key="user.userID">
 
-    <div class="col">
-      <h2 class="admin text-white">Product</h2>
-      <div class="row">
-        <router-link to="/AddProductModal">
-          <button class="btn btn-primary">Add Product</button>
-        </router-link>
-      </div>
-      <div class="container">
-        <table class="table table-striped-columns rounded-5">
-          <thead>
-            <tr>
-              <th scoped="col">Product ID</th>
-              <th scoped="col">Product Name</th>
-              <th scoped="col">Product Category</th>
-              <th scoped="col">Product Amount</th>
-              <th scoped="col">Action</th>
-            </tr>
-          </thead>
-          <tbody v-if="products">
-            <tr v-for="product in products" :key="product.prodID">
-              <td><img :src="product.prodUrl" style="height: 100px;" /></td>
-              <td>{{ product.prodName }}</td>
-              <td>{{ product.Category }}</td>
-              <td>R {{ product.amount }}</td>
-              <td>
-                <div class="row">
-                  <div class="col">
-                    <button @click="editProduct(product)" class="btn btn-primary btn-block">Edit</button>
-                  </div>
-                  <div class="col">
-                    <button @click="deleteProduct(product)" class="btn btn-danger btn-block">Delete</button>
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
+    <td>{{ user.userID }}</td>
+    <td>{{ user.firstName }}</td>
+    <td>{{ user.lastName }}</td>
+    <td>{{ user.userAge }}</td>
+    <td>{{ user.Gender }}</td>
+    <td>{{ user.userRole }}</td>
+    <td>{{ user.emailAdd }}</td>
+    <td>{{ user.userProfile }}</td>
+    <td class="d-flex justify-content-between">
+      <updateUser :user="user" />
+      <button class="btn btn-success deleteButton" @click="event => deleteUser(user.userID)">Delete</button>
+    </td>
+  </tr>
+</tbody>
+</table>
+</div>
+<!-- PRODUCTS TABLE -->
+<br><h2>Products Table</h2><br>
+<button class="btn btn-primary" @click="toggleAddProduct">Add Product</button>
+    <addProduct v-if="showAddProduct"/>
+  <div class="container table-responsive">
+    <table class="table">
+    <thead class="table-dark">
+      <tr>
+        <th>Product ID</th>
+        <th>Product Name</th>
+        <th>Quantity</th>
+        <th>Amount</th>
+        <th>Category</th>
+        <th>Product URL</th>
+        <th>Product Description</th>
+        <th>
+            Action
+        </th>
+    </tr>
+</thead>
+<tbody v-if="products">
+          <tr v-for="product in products" :key="product.prodID">
+            <td>{{ product.prodID }}</td>
+            <td>{{ product.prodName }}</td>
+            <td>{{ product.quantity }}</td>
+            <td>R {{ product.amount }}</td>
+            <td>{{ product.Category }}</td>
+            <td>{{ product.prodURL }}</td>
+            <td>{{ product.prodDesc }}</td>
+            <td class="d-flex justify-content-between">
+              <updateProduct :product="product" />
+              <button class="btn btn-primary editButton" @click="event => editProduct(product)">Edit</button>
+              <button class="btn btn-success deleteButton" @click="event => deleteProduct(product.prodID)">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+</table>
+</div>
+</div>
 </template>
-
 <script>
+import updateUser from '@/components/UpdateUserModal.vue';
+import updateProduct from '@/components/UpdateProduct.vue';
+import addProduct from '@/components/AddProduct.vue';
+import addUser from '@/components/AddUser.vue';
+
 export default {
+  components: {
+    updateUser,
+    updateProduct,
+    addProduct,
+    addUser,
+  },
+  data() {
+    return {
+      showAddUser: false,
+      showAddProduct: false,
+    };
+  },
   computed: {
-    users() {
-      return this.$store.state.users;
-    },
     products() {
       return this.$store.state.products;
     },
-  },
-  methods: {
-    editUser(user) {
-      console.log(`Editing user: ${user.userID}`);
-    },
-    deleteUser(user) {
-      this.$store.dispatch('deleteUser', { id: user.userID });
-    },
-    editProduct(product) {
-      console.log(`Editing product: ${product.prodID}`);
-    },
-    deleteProduct(product) {
-      this.$store.dispatch('deleteProduct', { id: product.prodID });
+    users() {
+      return this.$store.state.users;
     },
   },
   mounted() {
-    this.$store.dispatch('fetchUsers');
     this.$store.dispatch('fetchProducts');
+    this.$store.dispatch('fetchUsers');
+  },
+  methods: {
+    deleteUser(userID) {
+      this.$store.dispatch('deleteUser', { id: userID });
+    },
+    deleteProduct(prodID) {
+      this.$store.dispatch('deleteProduct', { id: prodID });
+    },
+    updateUser(user) {
+      let editedUser = {
+        userID: user.userID,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        userAge: user.userAge,
+        gender: user.Gender,
+        userRole: user.userRole,
+        emailAdd: user.emailAdd,
+        userPass: user.userPass,
+        userProfile: user.userProfile,
+      };
+
+      this.$store.dispatch('updateUser', { id: user.userID, data: editedUser });
+    },
+
+    updateProduct(product) {
+      let editedProduct = {
+        prodID: product.prodID,
+        prodName: product.prodName,
+        quantity: product.quantity,
+        amount: product.amount,
+        Category: product.Category,
+        prodUrl: product.prodUrl,
+      };
+
+      this.$store.dispatch('updateProduct', { id: product.prodID, data: editedProduct });
+    },
+
+    toggleAddUser() {
+      this.showAddUser = !this.showAddUser;
+    },
+
+    toggleAddProduct() {
+      this.showAddProduct = !this.showAddProduct;
+    },
   },
 };
 </script>
 
+
+
+
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Jacques+Francois+Shadow&display=swap');
-
-.rounded {
-  border-radius: 10px !important; 
+td {
+padding: 10px;
 }
-
-.adminpage {
-  overflow-x: hidden;
+@media(max-width:351px){
+td {
+  padding: 20px;
 }
-
-.admin {
-  font-family: 'Jacques Francois Shadow', cursive;
-  font-size: 5rem;
-}
-
-.subadmin {
-  font-family: 'Jacques Francois Shadow', cursive;
-  font-size: 5rem;
-  border-radius: 20px;
-}
-
-.container {
-  border-radius: 1px;
-  color: grey;
 }
 </style>
